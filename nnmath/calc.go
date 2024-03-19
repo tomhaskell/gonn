67f00e8c06ddb45b02e501aa5d8b54a9c8246824
Tom Haskell <tomfhaskell@gmail.com>
2024-03-19 19:27:06 +0000

    add sigmoidPrime func

--- nnmath/calc.go
+++ nnmath/calc.go
@@ -7,6 +7,11 @@ func Sigmoid(x float64) float64 {
 	return float64(1.0 / (1.0 + math.Exp(float64(-x))))
 }
 
+// SigmoidPrime returns the derivative of the sigmoid function
+func SigmoidPrime(x float64) float64 {
+	return Sigmoid(x) * (1 - Sigmoid(x))
+}
+
 // MeanSquaredError returns the average of the squared differences between the actual and expected values
 func MeanSquaredError(actual, expected []float64) float64 {
 	var sum float64
@@ -15,3 +20,4 @@ func MeanSquaredError(actual, expected []float64) float64 {
 	}
 	return sum / float64(len(actual))
 }
+
