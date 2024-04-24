ae72a12c64b471735909cb138cb832b1b89ee7d8
Tom Haskell <tom@tomhaskell.co.uk>
2024-04-24 11:08:21 +0100

    allow different activation types per layer in Builder

--- builder.go
+++ builder.go
@@ -4,30 +4,33 @@ import "github.com/tomhaskell/gonn/nn"
 
 // Builder is a Builder pattern for creating a new gonn neural network
 type Builder interface {
-	SetType(activationType string) Builder
+	SetDefaultActivation(activationType string) Builder
 	SetInputCount(numInputs int) Builder
 	AddLayer(numNeurons int) Builder
+	AddLayerWithActivation(numNeurons int, activationType string) Builder
 	Build() *Net
 }
 
 // NewBuilder creates a new Builder
 func NewBuilder() Builder {
 	return &netBuilder{
-		activation: nn.SIGMOID,
-		inputs: 0,
-		layers: make([]int, 0),
+		defaultActivation: nn.SIGMOID,
+		inputs:            0,
+		layers:            make([]int, 0),
+		activations:       make([]string, 0),
 	}
 }
 
 type netBuilder struct {
-	activation string
-	inputs int
-	layers []int
+	defaultActivation string
+	inputs            int
+	layers            []int
+	activations       []string
 }
 
 // SetType sets the activation function type for the neural network
-func (n netBuilder) SetType(activationType string) Builder {
-	n.activation = activationType
+func (n netBuilder) SetDefaultActivation(activationType string) Builder {
+	n.defaultActivation = activationType
 	return n
 }
 
@@ -36,9 +39,16 @@ func (n netBuilder) SetInputCount(numInputs int) Builder {
 	n.inputs = numInputs
 	return n
 }
-// AddLayer adds a layer to the neural network with the given number of neurons
+
+// AddLayer adds a layer to the neural network with the given number of neurons, using the default activation function
 func (n netBuilder) AddLayer(numNeurons int) Builder {
+	return n.AddLayerWithActivation(numNeurons, n.defaultActivation)
+}
+
+// AddLayerWithActivation adds a layer to the neural network with the given number of neurons, using the specified activation function
+func (n netBuilder) AddLayerWithActivation(numNeurons int, activationType string) Builder {
 	n.layers = append(n.layers, numNeurons)
+	n.activations = append(n.activations, activationType)
 	return n
 }
 
@@ -46,7 +56,7 @@ func (n netBuilder) Build() *Net {
 	l := make([]*nn.Layer, len(n.layers))
 	numInputs := n.inputs
 	for i, numNeurons := range n.layers {
-		l[i] = nn.NewLayer(n.activation, numNeurons, numInputs)
+		l[i] = nn.NewLayer(n.activations[i], numNeurons, numInputs)
 		numInputs = numNeurons
 	}
 	return NewNet(l, n.inputs)
